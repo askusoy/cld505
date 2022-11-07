4c91fba1c236822713ed76d4f2f980989bdd2ceb
askusoy <askusoy@hotmail.com>
2022-11-07 16:06:23 +1000

    version 1 of gorgeous cupcakes

---
+++ model/database.php
@@ -0,0 +1,23 @@
+<?php
+	//database connection details
+	$host = 'database-1.ccj6e1dsz8lm.us-east-1.rds.amazonaws.com'; // change this to endpoint from RDS URL 
+	$user = 'admin'; // might be different...
+	$password = 'awsbaru2022!'; // might be different as well...
+	$database = 'gorgeous_cupcakes';
+
+	//connect to database with a try/catch statement
+	//if the connection is not successful display the error message via database_error.php
+	//the PDOException class represents the error raised by PDO
+	//the PDO error is stored in the variable $e
+	//the PDO error is returned as a string via the getMessage() function
+	try
+	{
+		$conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+	}
+	catch(PDOException $e)
+	{
+		$error_message = $e->getMessage();
+		include('../view/database_error.php');
+		exit();
+	}
+?>
